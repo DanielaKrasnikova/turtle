8209a12fcf3ec114e7c2f462d75e784f898b4c8c
Dana Krasnikova <77327843+DanielaKrasnikova@users.noreply.github.com>
2022-02-01 13:56:40 +0000

    new work

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import turtle
 Rafael = turtle.Turtle()
 Rafael.shape("turtle")
-Rafael.color("green")
-Rafael.circle(100,steps=6)
-Rafael.forward(100)
-Rafael.color("blue")
-Rafael.circle(50)
-Rafael.right(90)
-Rafael.backward(50)
-Rafael.color("red")
-Rafael.circle(50)
+
+for i in range(8,12):
+  Rafael.circle(100)
+  Rafael.circle(100,steps=4)
+  Rafael.right(30)
